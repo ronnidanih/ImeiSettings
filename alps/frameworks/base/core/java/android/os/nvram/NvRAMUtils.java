ce3e6f53f5333b8e303b700e563f7a81ccaec2cb
qtuanye <qin_ty@chinaxuhu.com>
2017-02-17 09:37:11 +0800

    初始化IMEI号写入工具

---
+++ alps/frameworks/base/core/java/android/os/nvram/NvRAMUtils.java
@@ -0,0 +1,195 @@
+package android.os.nvram;
+
+import android.os.RemoteException;
+import android.os.ServiceManager;
+
+/**
+ * Created by TRF1309 on 2017-01-19.
+ */
+public class NvRAMUtils {
+    
+    private static final NvRAMAgent mAgent = NvRAMAgent.Stub.asInterface(ServiceManager.getService("NvRAMAgent"));
+    private static final String TAG = "NvRAMUtils";
+
+    public static final int UNIFIED_LID = 59;
+
+    /***** NvRAMUtils - Place your offset below *****/
+    //@Description: Ensure there is no duplicated offset declared!
+    // NV offset - Please Try to use the index between 1024 - 2047
+    public static final int NV_OFFSET = 1024;
+    
+    // Declared offsets above should be included in this integer array, otherwise verification and I/O afterwards will fail
+    private static final int[] INDEX_LIST = {
+    
+    };
+
+    /***** NvRAMUtils - Place your offset above *****/
+
+    /***** NvRAMUtils - Generic I/O Methods begin *****/
+    /**
+     * Read the specific index in NvRAM
+     * @param index the position that need to be accessed
+     * @return the byte written in the index, -1 when read failed
+     */
+    public synchronized static byte readNV(int index) throws RemoteException {
+        byte result = -1;
+        if(!verifyIndex(index)) {
+            android.util.Log.d(TAG,"Argument: " + index + "is not declared in INDEX_LIST! Read failed");
+            return result;
+        }
+        byte[] buff = readNV();
+        if (buff != null && index < buff.length) {
+            result = buff[index];
+        }
+        return result;
+    }
+    /**
+     * Read the specific range in NvRAM
+     * @param index the start index that need to be accessed
+     * @param length from the start index, the length of the range that need to be accessed
+     * @return the byte array written in the specified range, null when read failed
+     */
+    public synchronized static byte[] readNV(int index, int length) throws RemoteException {
+        if(!verifyIndex(index)) {
+            android.util.Log.d(TAG,"Argument: " + index + "is not declared in INDEX_LIST! Read failed");
+            return null;
+        }
+        byte[] buff = readNV();
+        byte[] target = new byte[length];
+        for(int i = 0 ; i < length; i++){
+            target[i] = buff[i+index];
+        }
+        return target;
+    }
+
+    /**
+     * Read all the data in NvRAM
+     * @return the byte array(usually with length of 2048) containing all the bytes in NvRAM, null when read failed.
+     */
+    public synchronized static byte[] readNV() throws RemoteException {
+        return mAgent.readFile(UNIFIED_LID);
+    }
+
+    /**
+     * write a specified byte in a specified position
+     * @param index the position that need to be written
+     * @param value the value that need to be written
+     * @return true when write succeeded, false when write failed
+     */
+    public synchronized static boolean writeNV(int index, byte value) throws RemoteException {
+        if(!verifyIndex(index)) {
+            android.util.Log.d(TAG,"Argument: " + index + "is not declared in INDEX_LIST! Write failed");
+            return false;
+        }
+        boolean result = false;
+        byte[] buff = readNV();
+        if (buff != null && index < buff.length) {
+            buff[index] = value;
+            result = writeNV(buff);
+        }
+        return result;
+    }
+
+    /**
+     * write a specified byte array into NvRAM, start with the specified index
+     * @param index the start position that need to be written
+     * @param buff the values that need to be written from the start position
+     * @return true when write succeeded, false when write failed
+     */
+    public synchronized static boolean writeNV(int index, byte[] buff) throws RemoteException {
+        if(!verifyIndex(index)) {
+            android.util.Log.d(TAG,"Argument: " + index + "is not declared in INDEX_LIST! Write failed");
+            return false;
+        }
+        boolean result = false;
+        byte[] origin = readNV();
+        if (buff != null && origin != null && (index + buff.length) < origin.length) {
+            for(int i = 0 ; i < buff.length; i++){
+                origin[index+i] = buff[i];
+            }
+            result = writeNV(origin);
+        }
+        return result;
+    }
+
+    /**
+     * overwrite all the data in NvRAM
+     * @param buff the byte array(usually with length of 2048) containing all the bytes in NvRAM
+     * @return true when write succeeded, false when write failed
+     */
+    public synchronized static boolean writeNV(byte[] buff) throws RemoteException {
+        boolean result = false;
+        if (buff != null) {
+            result = mAgent.writeFile(UNIFIED_LID, buff) > 0;
+        }
+        return result;
+    }
+
+    /**
+     * read and return an integer converted base on 4 continuous bytes
+     * @param index the starting position in NvRAM
+     * @return the integer value converted from 4 continuous bytes
+     */
+    public synchronized static int readInt(int index) throws RemoteException {
+        byte[] buff = readNV(index, 4);
+        if(buff == null || buff.length != 4){
+            return -1;
+        }
+        return bytes2Int(buff);
+    }
+
+    /**
+     * write an integer into 4 continuous bytes
+     * @param index the starting position in NvRAM
+     * @param value the integer value that need to be written
+     * @return true when write succeeded, false when write failed
+     */
+    public synchronized static boolean writeInt(int index, int value) throws RemoteException {
+        byte[] buff = int2Bytes(value);
+        return writeNV(index,buff);
+    }
+
+    /**
+     * Private method to convert from byte array into integer
+     * @param byteArray the target byte array that need to be converted
+     * @return the integer that converted from target byte array
+     */
+    private static int bytes2Int(byte[] byteArray) {
+        int result = 0;
+        if (byteArray.length != 4) {
+            return result;
+        }
+        for (byte byteVal : byteArray) {
+            result = (result << 8) | (byteVal & 255);
+        }
+        return result;
+    }
+
+    /**
+     * Private method to convert from integer into byte array
+     * @param intVal the target integer value that need to be converted
+     * @return the byte array that converted from target integer
+     */
+    private static byte[] int2Bytes(int intVal) {
+        byte[] result = new byte[4];
+        if (intVal == 0) {
+            return result;
+        }
+        for (int i = 0; i < result.length; i++) {
+            result[result.length - i - 1] = (byte) (intVal & 255);
+            intVal = intVal >> 8;
+        }
+        return result;
+    }
+
+    private synchronized static boolean verifyIndex(int index){
+        //ZDP - To check whether the index is declared in the Array INDEX_LIST
+        for(int i = 0 ; i < INDEX_LIST.length; i++){
+            if(INDEX_LIST[i] == index){
+                return true;
+            }
+        }
+        return false;
+    }
+    /***** NvRAMUtils - Generic I/O Methods end *****/
+}
---
+++ alps/teksun/build/device-xunhu.mk
@@ -0,0 +1,112 @@
+##########################################
+# Teksun Makefile Configuration
+#
+#  Author:      caicai
+#  Version:     1.0.0
+#  Date:        2016.12.09
+##########################################
+
+
+
+TEKSUN_TARGET_PROJECT=$(MTK_TARGET_PROJECT)
+$(warning caicai_debug TEKSUN_TARGET_PROJECT=$(TEKSUN_TARGET_PROJECT))
+#$(shell rm -rf out/buildinfo.txt)
+#$(shell echo TARGET_PRODUCT=$(TEKSUN_TARGET_PROJECT) > out/buildinfo.txt)
+#$(shell echo TARGET_BUILD_VARIANT=$(TARGET_BUILD_VARIANT) >> out/buildinfo.txt)
+$(shell test -d out || mkdir out)
+$(shell printenv | grep -E "ANDROID_BUILD_TOP|TARGET_PRODUCT|TARGET_BUILD_VARIANT|ANDROID_PRODUCT_OUT" > out/buildinfo.txt)
+
+
+# kernel config file configuration
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+$(shell cp device/teksun/$(TEKSUN_TARGET_PROJECT)/kernel-autoconfig/debug_defconfig  kernel-3.18/arch/arm/configs/kernel_defconfig)
+else
+$(shell cp device/teksun/$(TEKSUN_TARGET_PROJECT)/kernel-autoconfig/user_defconfig  kernel-3.18/arch/arm/configs/kernel_defconfig)
+endif
+KERNEL_DEFCONFIG := kernel_defconfig
+# end kernel config file configuration
+
+
+# MemoryDevice configuration
+MEMORY_TYPE := $(strip $(XUNHU_PROJECT_MEMORY_TYPE))
+#$(warning MEMORY_TYPE = $(MEMORY_TYPE))
+ifneq ($(wildcard vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/custom_MemoryDevice.h.$(MEMORY_TYPE)),)
+  $(shell cp -rf vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/custom_MemoryDevice.h.$(MEMORY_TYPE)  vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/$(TEKSUN_TARGET_PROJECT)/inc/custom_MemoryDevice.h )
+  PRODUCT_PROPERTY_OVERRIDES += ro.xunhu.memory_type=$(MEMORY_TYPE)
+else
+  $(error check your Projectconfig.mk :  XUNHU_PROJECT_MEMORY_TYPE or custom_MemoryDevice.h.$(MEMORY_TYPE) if exsit  )
+endif
+
+# end MemoryDevice configuration
+
+
+
+# begin: Add by caicai for During the first boot, the date display the default build time 
+$(shell echo "#define RTC_DEFAULT_MTH $(shell date +%-m)" > vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/$(TEKSUN_TARGET_PROJECT)/inc/make_month.h)
+$(shell echo "#define RTC_DEFAULT_YEA $(shell date +%Y)" >> vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/$(TEKSUN_TARGET_PROJECT)/inc/make_month.h)
+
+$(shell echo "#define RTC_DEFAULT_MTH $(shell date +%-m)" > kernel-3.18/drivers/misc/mediatek/rtc/make_month.h)
+$(shell echo "#define RTC_DEFAULT_YEA $(shell date +%Y)" >> kernel-3.18/drivers/misc/mediatek/rtc/make_month.h)
+# end Xunhu: Add by caicai for During the first boot, the date display the default build time
+
+
+
+# TEKSUN PREBUILT APP configuration BEGIN
+$(warning ======================腾瑞丰预装应用方案V1.0======================)
+$(shell bash teksun/prebuilt/build/prebuilt_mk.sh $(TEKSUN_TARGET_PROJECT))
+include teksun/prebuilt/build/prebuilt_app.mk
+
+ifneq ($(wildcard device/teksun/$(TEKSUN_TARGET_PROJECT)/custom/prebuilt_app.mk),)
+  include device/teksun/$(TEKSUN_TARGET_PROJECT)/custom/prebuilt_app.mk
+endif
+
+$(warning TEKSUN_OPERATOR_APP=$(TEKSUN_OPERATOR_APP))
+$(warning TEKSUN_PRIV_APP=$(TEKSUN_PRIV_APP))
+$(warning TEKSUN_APP=$(TEKSUN_APP))
+
+PRODUCT_PACKAGES += $(subst ",,$(strip $(TEKSUN_OPERATOR_APP)))
+PRODUCT_PACKAGES += $(subst ",,$(strip $(TEKSUN_PRIV_APP)))
+PRODUCT_PACKAGES += $(subst ",,$(strip $(TEKSUN_APP)))
+
+
+#$(warning $(PRODUCT_PACKAGES))
+$(warning ======================腾瑞丰预装应用方案END======================)
+# TEKSUN PREBUILT APP configuration END 
+
+#eng需加入测试版本水印，user版本也要用开关可以加入测试版本水印，防止未测试的软件客户用于量产
+ifeq ($(strip $(TARGET_BUILD_VARIANT)),eng)
+    PRODUCT_PACKAGES += MineVersion
+else 
+	ifeq ($(strip $(XUNHU_LH_OPEN_TEST_VERSION)), yes)
+	    PRODUCT_PACKAGES += MineVersion
+	endif
+endif
+
+#add GMS core app
+ifeq ($(strip $(XUNHU_BUILD_GMS)),yes) 
+PRODUCT_SHIPPING_API_LEVEL := 24
+$(call inherit-product-if-exists, teksun/packages/apps/Google/products/gms.mk)
+endif
+
+
+# add FactoryTest
+PRODUCT_PACKAGES += FactoryTest
+
+# add SecretCode
+PRODUCT_PACKAGES += SecretCode
+
+#add 外单销量统计
+ifeq ($(strip $(XUNHU_LIUT_SALE_TRACKER)), yes)
+PRODUCT_PACKAGES += MengSalestracker
+endif
+
+#Xunhu: add SmartWakeUp function at 2017-02-06 11:45:11 by QinTuanye{{&&
+#Description: 	开启智能唤醒后编译SmartWakeUp应用
+ifeq ($(strip $(XUNHU_QTY_SMART_WAKE_UP)), yes)
+	PRODUCT_PACKAGES += SmartWakeUp
+endif
+#&&}}
+
+#Xunhu: 未写IMEI或MEID需有未写IMEI号的水印，防止客户未写IMEI出货 at 2017-02-15 by TRF251{{&&
+	PRODUCT_PACKAGES += WriteImei
+#&&}}
